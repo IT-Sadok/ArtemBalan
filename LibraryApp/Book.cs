6f0104d320b741058ba204791bec8ed1ede06d83
Artem Balan <concnew@gmail.com>
2026-05-12 12:07:12 +0300

    Update Book.cs

--- LibraryApp/Book.cs
+++ LibraryApp/Book.cs
@@ -8,13 +8,14 @@ public class Book
     public int Id { get; set; }
     public BookStatus Status { get; set; }
 
-    public Book(string title = "Empty", string author = "None", int year = 0, int id = -1,
+    public Book(string title = "Default", string author = "None", int year = 0, int id = -1,
         BookStatus status = BookStatus.Free)
     {
         Title = title;
         Author = author;
         Year = year;
         Id = id;
+
         Status = status;
     }
 }
